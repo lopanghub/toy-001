4721e6107444a5be889c3f1597e3c8dcaf1fa86c
Hyeon Hak Lee <dustmain@gmail.com>
2024-02-16 18:22:11 +0900

    0216

--- mini2.js
+++ mini2.js
@@ -1,5 +1,30 @@
-// 사이드 바 css 는 나중에 우선 본 게임의 동작 원리를 구현해봅시다.
-// 각 테이블을 누르면 클릭 이벤트로 바둑알을 흑 > 백 순서로 놓는다.
+// 변수 설정
 let tableData = document.querySelectorAll('td');
 
-tableData.addevnetListner
+
+// fuction내에 i를 넣으니 i값이 초기화 되서 밖으로 뺌
+// 흑백을 차례로 두고 돌이 있으면 돌을 두지 않음
+let i = 0;
+tableData.forEach(td => {
+    td.addEventListener('click', rotate)
+})
+
+function rotate(e) {
+    if(!e.target.innerHTML){
+        if(i%2 === 0){
+            e.target.innerHTML='⚫'
+        } else {
+            e.target.innerHTML = '⚪';
+        }
+        i++;
+    }
+}
+
+// rowIndex와 cellIndex는 알았다
+// 위의 함수는 클릭시 함수니까 이건 클릭 함수와 별도로 돌아가게 해야한다
+// 자체계산이니까
+
+function checkAndMarkCells(cellIndex){
+    let rows = document.getElementsByTagName('tr');
+    
+}
